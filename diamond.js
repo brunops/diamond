4818ab6839c4f7a91ba4976f1e40353c601b5e8a
Bruno Sanches <brunopsanches@gmail.com>
2013-02-25 01:29:24 -0300

    Translate enunciate to english and reindent everything using spaces

--- diamond.js
+++ diamond.js
@@ -1,76 +1,78 @@
 /**
+ *  Given a letter print a diamond starting with 'A'
+ *  with the supplied letter at the widest point.
  *
- *  Dado uma letra ('A' a 'Z'), exiba um diamante iniciando em 'A' e 
- *    tendo a letra fornecida com o ponto mais distante.
- *  Por exemplo, dado a letra 'E' temos:
- *      A   
+ *  For example: print-diamond 'E' prints
+ *
+ *      A
  *     B B
  *    C   C
  *   D     D
- *  E       E 
- *   D     D 
+ *  E       E
+ *   D     D
  *    C   C
  *     B B
  *      A
- *   
- *  Dado a letra 'C' temos:
+ *
+ *  For example: print-diamond 'C' prints
+ *
  *    A
  *   B B
  *  C   C
  *   B B
- *    A  
+ *    A
  */
-function Diamond(letter) {
-    var ACode      = 'A'.charCodeAt(0),
-        letterCode = letter.charCodeAt(0);
+function printDiamond(letter) {
+  var ACode  = 'A'.charCodeAt(0),
+      letterCode = letter.charCodeAt(0);
 
-    var lettersSize = letterCode - ACode;
+  var lettersSize = letterCode - ACode;
 
-    for (var i = 0; i < lettersSize; i++) {
-        printLetter(getLetterFromCode(ACode + i));
+  for (var i = 0; i < lettersSize; i++) {
+    printLetter(getLetterFromCode(ACode + i));
+  }
+
+  for (var i = lettersSize; i >= 0; i--) {
+    printLetter(getLetterFromCode(ACode + i));
+  }
+
+  function getCodeFromLetter(charLetter) {
+    return charLetter.charCodeAt(0);
+  }
+
+  function getLetterFromCode(code) {
+    return String.fromCharCode(code);
+  }
+
+  function printLetter(charLetter) {
+    console.log(getSpacesBefore(charLetter) + getLetterText(charLetter));
+  }
+
+  function getSpacesBefore(charLetter) {
+    return getSpaces(letterCode - getCodeFromLetter(charLetter));
+  }
+
+  function getSpaces(size) {
+    var spacesSize = new Array(size + 1);
+    return spacesSize.join(' ');
+  }
+
+  function getLetterText(charLetter) {
+    var charLetterCode = getCodeFromLetter(charLetter);
+
+    if (charLetterCode === ACode) {
+      return 'A';
     }
-    
-    for (var i = lettersSize; i >= 0; i--) {
-        printLetter(getLetterFromCode(ACode + i));
-    }
-
-    function getCodeFromLetter(charLetter) {
-        return charLetter.charCodeAt(0);
-    }
-
-    function getLetterFromCode(code) {
-        return String.fromCharCode(code);
-    }
-
-    function printLetter(charLetter) {
-        console.log(getSpacesBefore(charLetter) + getLetterText(charLetter));
-    }
-
-    function getSpacesBefore(charLetter) {
-        return getSpaces(letterCode - getCodeFromLetter(charLetter));
-    }
-
-    function getSpaces(size) {
-        var spacesSize = new Array(size + 1);
-        return spacesSize.join(' ');
-    }
-
-    function getLetterText(charLetter) {
-        var charLetterCode = getCodeFromLetter(charLetter);
-
-        if (charLetterCode === ACode) {
-            return 'A';
-        }
-        else if (charLetterCode === getCodeFromLetter('B')) {
-            return 'B B';
-        }
-
-        var middleSpaces = getLetterNumber(charLetter) * 2 - 1;
-            
-        return charLetter + getSpaces(middleSpaces) + charLetter;
+    else if (charLetterCode === getCodeFromLetter('B')) {
+      return 'B B';
     }
 
-    function getLetterNumber(charLetter) {
-        return (getCodeFromLetter(charLetter) - ACode);
-    }
+    var middleSpaces = getLetterNumber(charLetter) * 2 - 1;
+
+    return charLetter + getSpaces(middleSpaces) + charLetter;
+  }
+
+  function getLetterNumber(charLetter) {
+    return (getCodeFromLetter(charLetter) - ACode);
+  }
 }
